917b36a047e6f39cb1b0c62b47d1a9c7ef92311c
Jan <aso.gdynia@gmail.com>
2018-01-13 17:58:39 +0100

    Logger comments

--- src/Logger/logger.cpp
+++ src/Logger/logger.cpp
@@ -1,62 +1,110 @@
 #include "include/Logger/logger.h"
 
+/**
+ * @brief Main method for singleton
+ * @return Static Logger instance
+ */
 Logger::Logger&Logger::Logger::getInstance()
 {
     static Logger instance;
     return instance;
 }
 
+/**
+ * @brief Dtor
+ */
 Logger::Logger::~Logger()
 {
 
 }
 
+/**
+ * @brief Main function to log message
+ * @param msg Message to be logged (without line breaks)
+ * @param level Log level from loglevel.h enum
+ */
 void Logger::Logger::log(std::string msg, LogLevel level)
 {
     this->consoleWrite(msg, level);
 }
 
+/**
+ * @brief Alias for logging errors
+ * @param msg Message to be logged (without line breaks)
+ */
 void Logger::Logger::error(std::string msg)
 {
     Logger::getInstance().consoleWrite(msg, LogLevel::ERROR);
 }
 
+/**
+ * @brief Alias for logging warnings
+ * @param msg Message to be logged (without line breaks)
+ */
 void Logger::Logger::warn(std::string msg)
 {
     Logger::getInstance().consoleWrite(msg, LogLevel::WARN);
 }
 
+/**
+ * @brief Alias for logging informations
+ * @param msg Message to be logged (without line breaks)
+ */
 void Logger::Logger::info(std::string msg)
 {
     Logger::getInstance().consoleWrite(msg, LogLevel::INFO);
 }
 
+/**
+ * @brief Alias for logging messages with "OK" status
+ * @param msg Message to be logged (without line breaks)
+ */
 void Logger::Logger::ok(std::string msg)
 {
     Logger::getInstance().consoleWrite(msg, LogLevel::OK);
 }
 
+/**
+ * @brief Alias for logging plain text rows
+ * @param msg Message to be logged (without line breaks)
+ */
 void Logger::Logger::text(std::string msg)
 {
     Logger::getInstance().consoleWrite(msg, LogLevel::TEXT);
 }
 
+/**
+ * @brief Alias for logging debugging informations
+ * @param msg Message to be logged (without line breaks)
+ */
 void Logger::Logger::debug(std::string msg)
 {
     Logger::getInstance().consoleWrite(msg, LogLevel::DEBUG);
 }
 
+/**
+ * @brief Ctor
+ */
 Logger::Logger::Logger()
 {
 
 }
 
+/**
+ * @brief Inside function used to write to STDOUT
+ * @param msg Message to be logged (without line breaks)
+ * @param level Log level from loglevel.h enum
+ */
 void Logger::Logger::consoleWrite(std::string msg, LogLevel level)
 {
     this->writeTag(level, true);
     std::cout << msg << std::endl;
 }
 
+/**
+ * @brief Change foreground text color, works only in UNIX console with ANSI codes
+ * @param level Log level from loglevel.h enum - color depends on it
+ */
 void Logger::Logger::setColor(LogLevel level)
 {
     #ifdef __linux__
@@ -82,11 +130,23 @@ void Logger::Logger::setColor(LogLevel level)
     #endif
 }
 
+/**
+ * @brief Resets console foreground text color, works only in UNIX console with ANSI codes
+ */
 void Logger::Logger::resetColor()
 {
+    #ifdef __linux__
+
     std::cout << "\033[0m";
+
+    #endif
 }
 
+/**
+ * @brief Writes tag before log message
+ * @param level Log level from loglevel.h enum
+ * @param colors Enables colors, available only in UNIX consoles with ANSI codes
+ */
 void Logger::Logger::writeTag(LogLevel level, bool colors)
 {
     if (level != TEXT)
